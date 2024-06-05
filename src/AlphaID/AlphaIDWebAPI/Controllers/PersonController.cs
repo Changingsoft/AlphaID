52c9de73b88b3070f0f9f006181c5f9b1c5c3726
Jonny Yang <63300503@qq.com>
2024-06-05 11:31:05 +0800

    改进兼容性。

--- src/AlphaID/AlphaIDWebAPI/Controllers/PersonController.cs
+++ src/AlphaID/AlphaIDWebAPI/Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 ﻿using AlphaIdPlatform;
 using AlphaIdPlatform.Security;
 using IdSubjects;
+using IdSubjects.Subjects;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,14 +29,18 @@ public class PersonController(
     /// <summary>
     ///     通过 UserName 获取指定用户的信息。
     /// </summary>
-    /// <param name="userName"></param>
+    /// <remarks>
+    /// 重要提示！为保持兼容性，该查询接口仍支持传入自然人的SubjectId，但在后续版本中将不再支持。
+    /// </remarks>
+    /// <param name="userName">用户名</param>
     /// <returns></returns>
     [HttpGet("{userName}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<PersonInfoModel>> GetUserInfoAsync(string userName)
     {
-        NaturalPerson? person = await personManager.FindByNameAsync(userName);
+        //hack 为保持兼容性，下一版本将不再支持SubjectId查询
+        NaturalPerson? person = await personManager.FindByNameAsync(userName) ?? await personManager.FindByIdAsync(userName);
         if (person == null)
             return NotFound();
 
@@ -103,10 +108,63 @@ public class PersonController(
     }
 
     /// <summary>
+    /// 查找某个自然人。
+    /// </summary>
+    /// <remarks>
+    /// 此接口将被废弃。应使用/api/Person/Suggestions。
+    /// </remarks>
+    /// <param name="keywords">关键词。可以通过手机号码、姓名汉字、姓名全拼</param>
+    /// <returns>返回匹配的自然人信息。如果匹配数量超过50条，则只返回前50条。</returns>
+    [Obsolete("不再支持，应使用Suggestions。")]
+    [HttpGet("Search/{keywords}")]
+    public Task<PersonSearchResult> Search(string keywords)
+    {
+        if (string.IsNullOrWhiteSpace(keywords))
+        {
+            return Task.FromResult(new PersonSearchResult([]));
+        }
+
+        keywords = keywords.Trim();
+        if (keywords.Length < 2)
+            return Task.FromResult(new PersonSearchResult([]));
+
+        if (MobilePhoneNumber.TryParse(keywords, out MobilePhoneNumber number))
+        {
+            var result = personManager.Users.SingleOrDefault(p => p.PhoneNumber == number.ToString());
+            if (result == null)
+                return Task.FromResult(new PersonSearchResult([]));
+
+            return Task.FromResult(new PersonSearchResult([new PersonInfoModel(result.Id, result.PersonName.FullName, result.PersonName.SearchHint, new Uri(_urlInfo.AuthCenterUrl, $"/People/{result.Id}/Avatar").ToString())]));
+        }
+
+        var pinyinSearchSet = personManager.Users.Where(p => p.PersonName.SearchHint!.StartsWith(keywords)).OrderBy(p => p.PersonName.SearchHint!.Length).ThenBy(p => p.PersonName.SearchHint);
+        int pinyinSearchSetCount = pinyinSearchSet.Count();
+        var pinyinSearchResult = new List<NaturalPerson>(pinyinSearchSet.Take(30));
+
+        var nameSearchSet = personManager.Users.Where(p => p.PersonName.FullName.StartsWith(keywords)).OrderBy(p => p.PersonName.FullName.Length).ThenBy(p => p.PersonName.FullName);
+        int nameSearchSetCount = nameSearchSet.Count();
+        var nameSearchResult = new List<NaturalPerson>(nameSearchSet.Take(30));
+
+        var searchResults = pinyinSearchResult.UnionBy(nameSearchResult, p => p.Id);
+
+        var final = new List<PersonInfoModel>();
+        foreach (var person in searchResults)
+        {
+            final.Add(new PersonInfoModel(person.Id, person.PersonName.FullName, person.PersonName.SearchHint, new Uri(_urlInfo.AuthCenterUrl, $"/People/{person.Id}/Avatar").ToString()));
+        }
+
+        return Task.FromResult(new PersonSearchResult(final, pinyinSearchSetCount > 30 || nameSearchSetCount > 30));
+    }
+
+    /// <summary>
     ///     获取指定用户的组织成员身份。
     /// </summary>
+    /// <remarks>
+    /// 重要提示！MembersOf 接口将被废弃。请使用 Memberships。目前仍支持通过指定SubjectId查找组成员身份，后续将不再支持。
+    /// </remarks>
     /// <param name="userName">用户名。</param>
     /// <returns></returns>
+    [HttpGet("{userName}/MembersOf")]
     [HttpGet("{userName}/Memberships")]
     public async Task<ActionResult<IEnumerable<MembershipModel>>> GetMemberships(string userName)
     {
@@ -115,7 +173,8 @@ public class PersonController(
         if (visitorSubjectId != null)
             visitor = await personManager.FindByIdAsync(User.SubjectId()!);
 
-        NaturalPerson? person = await personManager.FindByNameAsync(userName);
+        //hack 为保持兼容性，下一版本将不再支持SubjectId查询
+        NaturalPerson? person = await personManager.FindByNameAsync(userName) ?? await personManager.FindByIdAsync(userName);
         if (person == null)
             return new ActionResult<IEnumerable<MembershipModel>>([]);
 
@@ -134,7 +193,14 @@ public class PersonController(
         string SubjectId,
         string Name,
         string? SearchHint,
-        string? AvatarUrl);
+        string? AvatarUrl)
+    {
+        /// <summary>
+        /// 已废弃，将始终返回false.
+        /// </summary>
+        [Obsolete("不再使用此属性。")]
+        public bool RealNameValid { get; set; } = false;
+    };
 
     /// <summary>
     ///     自然人
@@ -186,4 +252,12 @@ public class PersonController(
         {
         }
     }
+
+    /// <summary>
+    /// 自然人搜索结果。
+    /// </summary>
+    /// <param name="Persons">自然人搜索的结果。</param>
+    /// <param name="More">指示一个值，表示该结果不完全，需要尝试更多的关键字来缩小搜索范围。</param>
+    [Obsolete("不再使用此结构。")]
+    public record PersonSearchResult(IEnumerable<PersonInfoModel> Persons, bool More = false);
 }

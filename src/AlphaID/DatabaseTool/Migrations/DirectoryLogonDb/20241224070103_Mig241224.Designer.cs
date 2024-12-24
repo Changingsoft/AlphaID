f150660394148bbaa66e1667ff72de7dff31b03e
Jonny Yang <63300503@qq.com>
2024-12-24 15:41:37 +0800

    添加和修整appsettings

--- src/AlphaID/DatabaseTool/Migrations/DirectoryLogonDb/20241224070103_Mig241224.Designer.cs
+++ src/AlphaID/DatabaseTool/Migrations/DirectoryLogonDb/20241224070103_Mig241224.Designer.cs
@@ -3,6 +3,7 @@ using AlphaId.DirectoryLogon.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatabaseTool.Migrations.DirectoryLogonDb
 {
     [DbContext(typeof(DirectoryLogonDbContext))]
-    partial class DirectoryLogonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241224070103_Mig241224")]
+    partial class Mig241224
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

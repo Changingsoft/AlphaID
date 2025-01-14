c1ee27b83734f4104907d97e917a807fd79c61e5
Jonny Yang <63300503@qq.com>
2025-01-15 02:29:35 +0800

    Remove unused projects.

--- src/AlphaID/Database/UpgradeScripts/Combine.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist ".\Release" md "Release"
-if exist ".\Release\Combined.sql" del ".\Release\Combined.sql"
-for %%f in (*.sql) do type "%%f" >> .\Release\Combined.sql
-echo success
-pause

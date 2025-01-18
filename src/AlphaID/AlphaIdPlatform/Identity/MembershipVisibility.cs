85ae41071dcd87f2640e630c8854a821fe3b11cc
Jonny Yang <63300503@qq.com>
2025-01-18 22:48:08 +0800

    Move MembershipVisibility to its correct location.

--- src/AlphaID/AlphaIdPlatform/Identity/MembershipVisibility.cs
+++ src/AlphaID/AlphaIdPlatform/Identity/MembershipVisibility.cs
@@ -1,4 +1,4 @@
-﻿namespace IdSubjects;
+namespace AlphaIdPlatform.Identity;
 
 /// <summary>
 ///     Membership Visibility
--- src/AlphaID/AlphaIdPlatform/Invitations/JoinOrganizationInvitation.cs
+++ src/AlphaID/AlphaIdPlatform/Invitations/JoinOrganizationInvitation.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using IdSubjects;
+using AlphaIdPlatform.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace AlphaIdPlatform.Invitations;

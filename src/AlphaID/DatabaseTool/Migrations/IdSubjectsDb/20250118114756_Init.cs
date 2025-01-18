7ae8df74d6c83c0df37881ca2663a4db6b27e481
Jonny Yang <63300503@qq.com>
2025-01-18 19:56:25 +0800

    Reset database migration.

--- src/AlphaID/DatabaseTool/Migrations/IdSubjectsDb/20250118114756_Init.cs
+++ src/AlphaID/DatabaseTool/Migrations/IdSubjectsDb/20250118114756_Init.cs
@@ -17,36 +17,11 @@ namespace DatabaseTool.Migrations.IdSubjectsDb
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
-                    PasswordLastSet = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
-                    WhenCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
-                    WhenChanged = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
-                    PersonWhenChanged = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
-                    Enabled = table.Column<bool>(type: "bit", nullable: false),
-                    PersonName_Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    PersonName_MiddleName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    PersonName_GivenName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    PersonName_FullName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
-                    PersonName_SearchHint = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
-                    NickName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
-                    Gender = table.Column<string>(type: "varchar(6)", nullable: true, comment: "性别"),
-                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: true),
                     Bio = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
                     PhoneticSurname = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
                     PhoneticGivenName = table.Column<string>(type: "varchar(40)", unicode: false, maxLength: 40, nullable: true),
-                    ProfilePicture_MimeType = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
-                    ProfilePicture_Data = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
-                    Locale = table.Column<string>(type: "varchar(10)", unicode: false, maxLength: 10, nullable: true),
-                    TimeZone = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
-                    Address_Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Region = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Locality = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Street1 = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Street2 = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Street3 = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Receiver = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Address_Contact = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
-                    Address_PostalCode = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
-                    WebSite = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    PersonWhenChanged = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    SearchHint = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
@@ -60,7 +35,32 @@ namespace DatabaseTool.Migrations.IdSubjectsDb
                     TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
                     LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
                     LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
-                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false),
+                    PasswordLastSet = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    WhenCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    WhenChanged = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false),
+                    FamilyName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    MiddleName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    GivenName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    NickName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Gender = table.Column<string>(type: "varchar(6)", nullable: true, comment: "性别"),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: true),
+                    ProfilePicture_MimeType = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    ProfilePicture_Data = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Locale = table.Column<string>(type: "varchar(10)", unicode: false, maxLength: 10, nullable: true),
+                    TimeZone = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Address_Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Region = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Locality = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Street1 = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Street2 = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Street3 = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Receiver = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address_Contact = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    Address_PostalCode = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    WebSite = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
                 },
                 constraints: table =>
                 {
@@ -369,16 +369,6 @@ namespace DatabaseTool.Migrations.IdSubjectsDb
                 name: "EmailIndex",
                 table: "ApplicationUser",
                 column: "NormalizedEmail");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ApplicationUser_PersonName_FullName",
-                table: "ApplicationUser",
-                column: "PersonName_FullName");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ApplicationUser_PersonName_SearchHint",
-                table: "ApplicationUser",
-                column: "PersonName_SearchHint");
 
             migrationBuilder.CreateIndex(
                 name: "IX_ApplicationUser_WhenChanged",

4e6fdb99b999172ac9c4d462c0ae3015d4fb41c7
Jonny Yang <63300503@qq.com>
2025-02-27 16:08:27 +0800

    修复外部登录始终不成功。

--- src/AlphaID/AuthCenterWebApp/Pages/ExternalLogin/Callback.cshtml.cs
+++ src/AlphaID/AuthCenterWebApp/Pages/ExternalLogin/Callback.cshtml.cs
@@ -28,7 +28,7 @@ public class Callback(
     {
         //从外部登录认证
         AuthenticateResult result =
-            await HttpContext.AuthenticateAsync(IdentityServerConstants.ExternalCookieAuthenticationScheme);
+            await HttpContext.AuthenticateAsync(IdentityConstants.ExternalScheme);
         if (result.Succeeded != true) throw new Exception("外部登录错误。");
 
         ClaimsPrincipal? externalUser = result.Principal;

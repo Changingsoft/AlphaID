dcb93fed44e20e293cb92c1b5b779a5420e386d9
Jonny Yang <63300503@qq.com>
2025-03-03 09:40:15 +0800

    改进搜索。

--- src/AlphaID/AdminWebApp/Areas/UserManagement/Pages/Search.cshtml.cs
+++ src/AlphaID/AdminWebApp/Areas/UserManagement/Pages/Search.cshtml.cs
@@ -1,13 +1,20 @@
 using AlphaIdPlatform.Identity;
 using IdSubjects;
 using IdSubjects.Subjects;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdminWebApp.Areas.UserManagement.Pages;
 
-public class SearchModel(ApplicationUserManager<NaturalPerson> personManager) : PageModel
+public class SearchModel(IUserStore<NaturalPerson> store) : PageModel
 {
-    public IEnumerable<NaturalPerson> Results { get; set; } = [];
+    public IEnumerable<SearchedUser> Results { get; set; } = [];
+
+    public IQueryableUserStore<NaturalPerson> Store { get; set; } = store as IQueryableUserStore<NaturalPerson> ??
+                                                                    throw new NotSupportedException("不支持查询用户。");
+
+    public IApplicationUserStore<NaturalPerson> ApplicationUserStore { get; set; } = store as IApplicationUserStore<NaturalPerson> ??
+                                                                                 throw new NotSupportedException("不支持ApplicationStore。");
 
     public async Task<IActionResult> OnGetAsync(string q)
     {
@@ -17,17 +24,46 @@ public class SearchModel(ApplicationUserManager<NaturalPerson> personManager) : 
         if (MobilePhoneNumber.TryParse(q, out MobilePhoneNumber mobile))
         {
             NaturalPerson? person =
-                await personManager.FindByMobileAsync(mobile.ToString(), HttpContext.RequestAborted);
+                await ApplicationUserStore.FindByPhoneNumberAsync(mobile.ToString(), HttpContext.RequestAborted);
             return person != null ? RedirectToPage("Detail/Index", new { id = person.Id }) : Page();
         }
 
-        var pinyinResult = new List<NaturalPerson>(personManager.Users
-            .Where(p => p.SearchHint!.StartsWith(q)).OrderBy(p => p.SearchHint!.Length)
-            .ThenBy(p => p.SearchHint));
-        var nameResult = new List<NaturalPerson>(personManager.Users.Where(p => p.Name!.StartsWith(q))
-            .OrderBy(p => p.Name!.Length).ThenBy(p => p.Name));
+        Results = from user in Store.Users
+                  where user.SearchHint!.StartsWith(q) || user.Name!.StartsWith(q) || user.UserName!.StartsWith(q) || user.Email!.StartsWith(q)
+                  select new SearchedUser()
+                  {
+                      Id = user.Id,
+                      Name = user.Name,
+                      UserName = user.UserName,
+                      PhoneNumber = user.PhoneNumber,
+                      PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                      Email = user.Email,
+                      EmailConfirmed = user.EmailConfirmed,
+                      Gender = user.Gender,
+                      DateOfBirth = user.DateOfBirth,
+                  };
 
-        Results = pinyinResult.UnionBy(nameResult, p => p.Id);
         return Page();
+    }
+
+    public class SearchedUser
+    {
+        public string Id { get; set; } = null!;
+
+        public string? Name { get; set; }
+
+        public string? UserName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public string? Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public DateOnly? DateOfBirth { get; set; }
     }
 }

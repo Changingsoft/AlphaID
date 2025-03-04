bc252d152e94ae2a9f97643f3112a4794ec5f8cc
Jonny Yang <63300503@qq.com>
2025-03-04 11:22:24 +0800

    改进组织列表查询。

--- src/AlphaID/AdminWebApp/Areas/OrganizationManagement/Pages/Index.cshtml.cs
+++ src/AlphaID/AdminWebApp/Areas/OrganizationManagement/Pages/Index.cshtml.cs
@@ -1,16 +1,38 @@
 using AlphaIdPlatform.Subjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdminWebApp.Areas.OrganizationManagement.Pages;
 
 public class IndexModel(IOrganizationStore organizationStore) : PageModel
 {
-    public IEnumerable<Organization> Last10Orgs { get; set; } = null!;
+    public IEnumerable<OrganizationModel> Last10Orgs { get; set; } = null!;
 
     public int OrgCount { get; set; }
 
     public void OnGet()
     {
-        Last10Orgs = organizationStore.Organizations.OrderByDescending(p => p.WhenChanged).Take(10);
+        Last10Orgs = (from org in organizationStore.Organizations.AsNoTracking()
+                      orderby org.WhenCreated descending
+                      select new OrganizationModel
+                      {
+                          Id = org.Id,
+                          Name = org.Name,
+                          Domicile = org.Domicile,
+                          Contact = org.Contact,
+                          Representative = org.Representative
+                      }).Take(10).ToList();
         OrgCount = organizationStore.Organizations.Count();
+    }
+
+    public class OrganizationModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Domicile { get; set; }
+
+        public string? Contact { get; set; }
+        public string? Representative { get; set; }
     }
 }

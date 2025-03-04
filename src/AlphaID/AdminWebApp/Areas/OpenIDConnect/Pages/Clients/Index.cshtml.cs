585e12eca89c25acc1b184f7413743c9284cc67f
Jonny Yang <63300503@qq.com>
2025-03-04 11:22:04 +0800

    改进oidc查询。

--- src/AlphaID/AdminWebApp/Areas/OpenIDConnect/Pages/Clients/Index.cshtml.cs
+++ src/AlphaID/AdminWebApp/Areas/OpenIDConnect/Pages/Clients/Index.cshtml.cs
@@ -1,18 +1,43 @@
 using Duende.IdentityServer.EntityFramework.DbContexts;
-using Duende.IdentityServer.EntityFramework.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdminWebApp.Areas.OpenIDConnect.Pages.Clients;
 
 public class IndexModel(ConfigurationDbContext context) : PageModel
 {
-    public IEnumerable<Client> Clients { get; set; } = null!;
+    public IEnumerable<ClientModel> Clients { get; set; } = null!;
 
     public void OnGet()
     {
-        Clients = context.Clients
-            .Include(p => p.AllowedGrantTypes)
-            .Include(p => p.RedirectUris)
-            .Include(p => p.AllowedScopes);
+        Clients = from client in context.Clients.AsNoTracking()
+                .Include(p => p.AllowedGrantTypes)
+                .Include(p => p.AllowedScopes)
+            select new ClientModel()
+            {
+                Id = client.Id,
+                ClientId = client.ClientId,
+                ClientName = client.ClientName,
+                RequireClientSecret = client.RequireClientSecret,
+                RequirePkce = client.RequirePkce,
+                AllowedGrantTypes = client.AllowedGrantTypes.Select(p => p.GrantType),
+                AllowedScopes = client.AllowedScopes.Select(p => p.Scope),
+            };
+    }
+
+    public class ClientModel
+    {
+        public int Id { get; set; }
+
+        public string ClientId { get; set; } = null!;
+
+        public string ClientName { get; set; } = null!;
+
+        public bool RequireClientSecret { get; set; }
+
+        public bool RequirePkce { get; set; }
+
+        public IEnumerable<string> AllowedGrantTypes { get; set; } = null!;
+
+        public IEnumerable<string> AllowedScopes { get; set; } = null!;
     }
 }

e6a64bd7c7edd613774f13f078583ba933571281
Jonny Yang <63300503@qq.com>
2025-03-13 00:46:19 +0800

    创建表示radius packet的有关结构体。

---
+++ src/Radius/RadiusCore/Packet/RadiusPacketStructExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RadiusCore.Packet
+{
+    /// <summary>
+    /// Extension methods for RadiusPacketStruct
+    /// </summary>
+    public static class RadiusPacketStructExtensions
+    {
+        /// <summary>
+        /// Convert a RadiusPacketStruct and a list of RadiusAttributes to a byte array
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <param name="attributes"></param>
+        /// <returns></returns>
+        public static byte[] ToByteArray(this RadiusPacketStruct packet, List<RadiusAttribute> attributes)
+        {
+            int size = Marshal.SizeOf(packet) + attributes.Count * Marshal.SizeOf(typeof(RadiusAttribute));
+            byte[] arr = new byte[size];
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+
+            try
+            {
+                Marshal.StructureToPtr(packet, ptr, true);
+                Marshal.Copy(ptr, arr, 0, Marshal.SizeOf(packet));
+
+                int offset = Marshal.SizeOf(packet);
+                foreach (var attribute in attributes)
+                {
+                    Marshal.StructureToPtr(attribute, ptr + offset, true);
+                    Marshal.Copy(ptr + offset, arr, offset, Marshal.SizeOf(attribute));
+                    offset += Marshal.SizeOf(attribute);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return arr;
+        }
+
+        /// <summary>
+        /// Convert a byte array to a RadiusPacketStruct and a list of RadiusAttributes
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="attributes"></param>
+        /// <returns></returns>
+        public static RadiusPacketStruct FromByteArray(byte[] data, out List<RadiusAttribute> attributes)
+        {
+            RadiusPacketStruct packet = new RadiusPacketStruct();
+            int size = Marshal.SizeOf(packet);
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+
+            try
+            {
+                Marshal.Copy(data, 0, ptr, size);
+                packet = (RadiusPacketStruct)Marshal.PtrToStructure(ptr, packet.GetType())!;
+
+                attributes = new List<RadiusAttribute>();
+                int offset = size;
+                while (offset < data.Length)
+                {
+                    RadiusAttribute attribute = new RadiusAttribute();
+                    int attrSize = Marshal.SizeOf(attribute);
+                    IntPtr attrPtr = Marshal.AllocHGlobal(attrSize);
+
+                    try
+                    {
+                        Marshal.Copy(data, offset, attrPtr, attrSize);
+                        attribute = (RadiusAttribute)Marshal.PtrToStructure(attrPtr, attribute.GetType())!;
+                        attributes.Add(attribute);
+                        offset += attribute.Length;
+                    }
+                    finally
+                    {
+                        Marshal.FreeHGlobal(attrPtr);
+                    }
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return packet;
+        }
+    }
+}

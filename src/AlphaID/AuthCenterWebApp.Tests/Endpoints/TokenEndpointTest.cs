be05b01ecc42720440b38f9852ca1aa649040dee
Jonny Yang <63300503@qq.com>
2025-06-27 10:52:18 +0800

    移除已不需要的测试。

--- src/AlphaID/AuthCenterWebApp.Tests/Endpoints/TokenEndpointTest.cs
+++ src/AlphaID/AuthCenterWebApp.Tests/Endpoints/TokenEndpointTest.cs
@@ -8,40 +8,6 @@ namespace AuthCenterWebApp.Tests.Endpoints;
 public class TokenEndpointTest(AuthCenterWebAppFactory factory)
 {
     [Fact]
-    public async Task GrantByClientCredentials()
-    {
-        HttpClient client = factory.CreateClient();
-        var forms = new Dictionary<string, string>
-        {
-            { "client_id", "d70700eb-c4d8-4742-a79a-6ecf2064b27c" },
-            { "client_secret", "i7zcwJu)5pgIA()huJWRoT@oCLHpwfe^" },
-            { "grant_type", "client_credentials" }
-        };
-        HttpResponseMessage response = await client.PostAsync("/connect/token", new FormUrlEncodedContent(forms));
-        response.EnsureSuccessStatusCode();
-        var tokenData = await response.Content.ReadFromJsonAsync<TokenResponse>();
-        Assert.Equal("Bearer", tokenData!.TokenType);
-    }
-
-    [Fact]
-    public async Task GrantByPasswordOwner()
-    {
-        HttpClient client = factory.CreateClient();
-        var forms = new Dictionary<string, string>
-        {
-            { "client_id", "d70700eb-c4d8-4742-a79a-6ecf2064b27c" },
-            { "client_secret", "i7zcwJu)5pgIA()huJWRoT@oCLHpwfe^" },
-            { "username", "liubei" },
-            { "password", "Pass123$" },
-            { "grant_type", "password" }
-        };
-        HttpResponseMessage response = await client.PostAsync("/connect/token", new FormUrlEncodedContent(forms));
-        response.EnsureSuccessStatusCode();
-        var tokenData = await response.Content.ReadFromJsonAsync<TokenResponse>();
-        Assert.Equal("Bearer", tokenData!.TokenType);
-    }
-
-    [Fact]
     public async Task GetTokenRateLimitTest()
     {
         var client = factory.CreateClient();

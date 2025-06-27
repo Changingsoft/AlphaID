22927f70f98d7660ff7130b58c5dc7d33aaddd67
Jonny Yang <63300503@qq.com>
2025-06-27 15:11:29 +0800

    修正闰年2月29日生日的计算错误。

--- src/AlphaID/AspNetWebLib.Tests/DateTimeExtensionsTest.cs
+++ src/AlphaID/AspNetWebLib.Tests/DateTimeExtensionsTest.cs
@@ -17,4 +17,21 @@ public class DateTimeExtensionsTest
         var now = new DateTime(2023, 4, 21);
         Assert.Equal(38, dateOfBirth.AsAge(now));
     }
+
+    [Fact]
+    public void AgeCalcWhenBornInLeapYear()
+    {
+        var dateOfBirth = new DateTime(1988, 2, 29); // Leap year
+        var now = new DateTime(2023, 2, 28); // Day before leap day
+        Assert.Equal(34, dateOfBirth.AsAge(now));
+
+        var now2 = new DateTime(2023, 3, 1);
+        Assert.Equal(35, dateOfBirth.AsAge(now2));
+
+        var now3 = new DateTime(2024, 2, 29); // Next leap year
+        Assert.Equal(35, dateOfBirth.AsAge(now3));
+
+        var now4 = new DateTime(2024, 3, 1);
+        Assert.Equal(36, dateOfBirth.AsAge(now4));
+    }
 }
--- src/AlphaID/AspNetWebLib/DateTimeExtensions.cs
+++ src/AlphaID/AspNetWebLib/DateTimeExtensions.cs
@@ -65,6 +65,9 @@ public static class DateTimeExtensions
     {
         //2017年10月1日施行的《民法总则》第二百零一条第一款规定：“按照年、月、日计算期间的，开始的当日不计入，自下一日开始计算
         int year = now.Year - dateOfBirth.Year;
+        //如果是平年但2月29日出生，则将其设置为2月28日。
+        if (dateOfBirth is { Month: 2, Day: 29 } && !DateTime.IsLeapYear(now.Year))
+            dateOfBirth = new DateOnly(dateOfBirth.Year, 2, 28);
         var anniversary = new DateOnly(now.Year, dateOfBirth.Month, dateOfBirth.Day); //取得当年的纪念日。
         if (now > anniversary)
             return year;
